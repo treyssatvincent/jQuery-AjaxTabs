46ed661ad79485b833ea59b46a6ee04e464cd26d
Treyssat-Vincent Nino <nino+git@treyssatvincent.fr>
2019-09-19 17:30:07 +0200

    tec: add jshint to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,19 @@
 
 const gulp = require('gulp'),
     plumber = require('gulp-plumber'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
     uglify = require('gulp-uglify'),
 
     src = './lib/jquery.ajaxtabs.js',
     dest = './dist/';
+
+function check() {
+    return gulp.src(src)
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish))
+        .pipe(jshint.reporter('fail'))
+}
 
 function minifyJs() {
     console.log('minify');
@@ -15,4 +24,5 @@ function minifyJs() {
         .pipe(gulp.dest(dest));
 }
 
-gulp.task('default', minifyJs);
+gulp.task('check', check);
+gulp.task('default', gulp.series(check, minifyJs));
